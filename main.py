14d36dd23d585a4532738c08d73b0b0b6891c826
CodeWithGamez <codewithgamez@gmail.com>
2020-08-16 21:12:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Comment my code of VARIABLES AND TYPES
+
+
+#1 
+
+greetings = "Hello, world"
+print("greetings")
+print(greetings)
+
+
+
+#2
+
+number_1 = 1
+number_2 = "two"
+print(type(number_1))
+print(type(number_2))
+
+
+#3
+
+number_3 = 2.0
+print(number_3)
+print(type(number_3))
+
